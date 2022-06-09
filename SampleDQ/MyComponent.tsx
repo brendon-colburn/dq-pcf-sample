59586ee125798b92abf78d8b07bc9012c2b05a8a
Brendon <brendon.colburn@gmail.com>
2022-06-09 14:55:19 -0400

    added a simple view model

---
+++ SampleDQ/MyComponent.tsx
@@ -0,0 +1,5 @@
+import { Label } from "@fluentui/react";
+import * as React from "react";
+
+const MyComponent = (props: any) => <Label>Record Count: {props.value}</Label>
+export default MyComponent
--- SampleDQ/index.ts
+++ SampleDQ/index.ts
@@ -1,56 +1,69 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
 import DataSetInterfaces = ComponentFramework.PropertyHelper.DataSetApi;
 type DataSet = ComponentFramework.PropertyTypes.DataSet;
+import * as React from "react";
+import MyComponent from "./MyComponent";
+import { render } from "react-dom";
 
-export class SampleDQ implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+export class SampleDQ
+  implements ComponentFramework.StandardControl<IInputs, IOutputs>
+{
+  /**
+   * Empty constructor.
+   */
+  constructor() {}
 
-    /**
-     * Empty constructor.
-     */
-    constructor()
-    {
+  /**
+   * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+   * Data-set values are not initialized here, use updateView.
+   * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+   * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+   * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+   * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+   */
+  public init(
+    context: ComponentFramework.Context<IInputs>,
+    notifyOutputChanged: () => void,
+    state: ComponentFramework.Dictionary,
+    container: HTMLDivElement
+  ): void {
+    context.webAPI
+      .retrieveMultipleRecords("cr56f_InspectionFormLineItem")
+      .then((response: ComponentFramework.WebApi.RetrieveMultipleResponse) => {
+        // Retrieve Multiple Web API call completed successfully
+        let count1 = 0;
+        for (const entity of response.entities) {
+          count1++;
+        }
+        const el = React.createElement(MyComponent, { value: count1 });
+        render(el, container);
+      }).catch(()=> {
+        const el = React.createElement(MyComponent, { value: "n/a" });
+        render(el, container);
+      });
+  }
 
-    }
+  /**
+   * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+   * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+   */
+  public updateView(context: ComponentFramework.Context<IInputs>): void {
+    // Add code to update control view
+  }
 
-    /**
-     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-     * Data-set values are not initialized here, use updateView.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-     * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
-     */
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
-    {
-        context.webAPI.retrieveMultipleRecords('cr56f_InspectionFormLineItem')
-    }
+  /**
+   * It is called by the framework prior to a control receiving new data.
+   * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+   */
+  public getOutputs(): IOutputs {
+    return {};
+  }
 
-
-    /**
-     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-     */
-    public updateView(context: ComponentFramework.Context<IInputs>): void
-    {
-        // Add code to update control view
-    }
-
-    /**
-     * It is called by the framework prior to a control receiving new data.
-     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
-     */
-    public getOutputs(): IOutputs
-    {
-        return {};
-    }
-
-    /**
-     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-     * i.e. cancelling any pending remote calls, removing listeners, etc.
-     */
-    public destroy(): void
-    {
-        // Add code to cleanup control if necessary
-    }
-
+  /**
+   * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+   * i.e. cancelling any pending remote calls, removing listeners, etc.
+   */
+  public destroy(): void {
+    // Add code to cleanup control if necessary
+  }
 }
